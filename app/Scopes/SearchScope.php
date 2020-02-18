03c4c1184d3483ab9d3d492581be5b0efd676e96
MadWeb <agencymadweb@gmail.com>
2020-02-18 11:28:21 -0800

    5m45s into video; searching under company persists company selection but not strict results, adds contacts with search result to company contact list; VS mystery problem closing tag on FilterScope.php

--- app/Scopes/SearchScope.php
+++ app/Scopes/SearchScope.php
@@ -8,15 +8,27 @@ use Illuminate\Database\Eloquent\Builder;
 
 class SearchScope implements Scope
 {
+    protected $searchColumns = ['first_name', 'last_name', 'email', 'company.name'];
+
     public function apply(Builder $builder, Model $model)
     {
-        if ($search = request('search')) {
-            $builder->where('first_name', 'LIKE', "%{$search}%")
-                ->orWhere('last_name', 'LIKE', "%{$search}%")
-                ->orWhere('email', 'LIKE', "%{$search}%");
-            $builder->orWhereHas('company', function($query) use ($search) {
-                $query->where('name', 'LIKE', "%{$search}%");
-            });
+        if ($search = request('search')) 
+        {
+            foreach ($this->searchColumns as $column) 
+            {
+                $arr = explode('.', $column);
+                if (count($arr) == 2) 
+                {
+                    list($relationship, $col) = $arr;
+
+                    $builder->orWhereHas($relationship, function($query) use ($search, $col) {
+                        $query->where($col, 'LIKE', "%{$search}%");
+                    });
+                }
+                else {
+                    $builder->orWhere($column, 'LIKE', "%{$search}%");
+                }
+            }
         }
     }
 }
